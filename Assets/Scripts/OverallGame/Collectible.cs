39e896964b29650de127465a8e3f980a2b4195b2
Rasser23 <uk38kq@student.aau.dk>
2025-12-06 18:05:49 +0100

    collectible script added comments

--- Assets/Scripts/OverallGame/Collectible.cs
+++ Assets/Scripts/OverallGame/Collectible.cs
@@ -2,37 +2,38 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Collectible : MonoBehaviour
+public class Collectible : MonoBehaviour // this script handles the logic of the collectibles 
 {
 
-    public GameObject pickupText;
-    private bool playerIsNear = false;
+    public GameObject pickupText; // UI text shown when player is close to object 
+    private bool playerIsNear = false; // tracks whether the player is in distance of pick up 
 
     private void Start()
     {
-        if(pickupText !=null)
+        if(pickupText !=null) // hide the pick up text at the start if it exists 
             pickupText.SetActive(false);
     }
 
     void Update()
     {
-        if(playerIsNear && Input.GetKeyDown(KeyCode.E))
+        if(playerIsNear && Input.GetKeyDown(KeyCode.E)) // if the player is close enough and presses E 
         {
-            if(pickupText != null)
+            // hide the text before destroying the object 
+            if(pickupText != null) 
                 pickupText.SetActive(false);
 
-            FindObjectOfType<GameManager>().AddCollectible();
+            FindObjectOfType<GameManager>().AddCollectible(); // tell the gamemanager that an collectible has been collected
             
-            Destroy(gameObject);
+            Destroy(gameObject); // remove the gameobject from the scene 
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) // when something enters the trigger area, check the tag 
         {
-            playerIsNear = true;
-            if (pickupText != null)
+            playerIsNear = true; // player is in range 
+            if (pickupText != null) // show the UI pick up text 
                 pickupText.SetActive(true);
             
         }
@@ -40,10 +41,10 @@ public class Collectible : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) // when the player leaves the trigger area 
         {
-            playerIsNear = false;
-            if (pickupText != null)
+            playerIsNear = false; // player is out of range 
+            if (pickupText != null) // hide the UI pickup text 
                 pickupText.SetActive(false);
         }
     }
