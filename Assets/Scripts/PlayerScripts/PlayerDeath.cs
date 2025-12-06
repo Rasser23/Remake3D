53b3fa2b7aaef222b1c6e944a19cca574ee40fb5
Rasser23 <uk38kq@student.aau.dk>
2025-12-06 17:42:22 +0100

    PlayerDeath comments added to script

--- Assets/Scripts/PlayerScripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerScripts/PlayerDeath.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerDeath : MonoBehaviour
+public class PlayerDeath : MonoBehaviour // this script handles the players death when touching the spheres
 {
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision) // If the player collides with gameobject with tag "Enemy" - restart the game 
     {
         if (collision.collider.CompareTag("Enemy"))
         {
@@ -12,7 +12,7 @@ public class PlayerDeath : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // same as before but fir trigger colliders
     {
         if (other.CompareTag("Enemy"))
         {
@@ -22,8 +22,8 @@ public class PlayerDeath : MonoBehaviour
 
     void RestartLevel()
     {
-        Scene current = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(current.buildIndex);
+        Scene current = SceneManager.GetActiveScene(); // get the current active scene 
+        SceneManager.LoadScene(current.buildIndex); // reload the scene via build.index
     }
 
 
